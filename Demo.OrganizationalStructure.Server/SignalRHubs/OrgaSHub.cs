08cff086507f07123f702cbe9432249151bbdb3b
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-22 23:43:43 +0200

    Implement create, update, delete operations of JobRole data model

--- Demo.OrganizationalStructure.Server/SignalRHubs/OrgaSHub.cs
+++ Demo.OrganizationalStructure.Server/SignalRHubs/OrgaSHub.cs
@@ -1,4 +1,5 @@
-﻿using Demo.OrganizationalStructure.Common.HubInterfaces;
+﻿using Demo.OrganizationalStructure.Common.DataModel;
+using Demo.OrganizationalStructure.Common.HubInterfaces;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
@@ -9,6 +10,21 @@ namespace Demo.OrganizationalStructure.Server.SignalRHubs
         public async Task PingDemo()
         {
             await Clients.All.SendAsync(nameof(IOrgaSHubClient.InvokePongDemo));
+        }
+
+        public async Task CreateJobRole(JobRole jobRole)
+        {
+            await Clients.Others.SendAsync(nameof(IOrgaSHubClient.InvokeCreateJobRole), jobRole);
+        }
+
+        public async Task UpdateJobRole(JobRole jobRole)
+        {
+            await Clients.All.SendAsync(nameof(IOrgaSHubClient.InvokeUpdateJobRole), jobRole);
+        }
+
+        public async Task DeleteJobRole(JobRole jobRole)
+        {
+            await Clients.All.SendAsync(nameof(IOrgaSHubClient.InvokeDeleteJobRole), jobRole);
         }
     }
 }
