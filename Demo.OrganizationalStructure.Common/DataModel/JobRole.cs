992fb71e2cf2802b9d53b306b1b1c36765a469d4
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-22 23:37:08 +0200

    Declare JobRole data model

---
+++ Demo.OrganizationalStructure.Common/DataModel/JobRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Demo.OrganizationalStructure.Common.DataModel
+{
+    public class JobRole
+    {
+        public Guid EntityKey { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
