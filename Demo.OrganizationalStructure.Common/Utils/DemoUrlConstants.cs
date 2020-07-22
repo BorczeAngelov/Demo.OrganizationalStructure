cfcf8b1527c6dfbb349330c16be5180998c50b60
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-22 22:14:01 +0200

    Add class containing hardcoded URLs

---
+++ Demo.OrganizationalStructure.Common/Utils/DemoUrlConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.OrganizationalStructure.Common.Utils
+{
+    /// <summary>
+    /// Demo class containing hardcoded URLs.
+    /// Note: In real project these values should be defined in .config files or etc.
+    /// </summary>
+    public static class DemoUrlConstants
+    {
+        public static string LocalHostUrl { get => "http://localhost:5000"; }
+        public static string OrgaSHubEndpoint { get => "/OrgaSHub"; }
+    }
+}
