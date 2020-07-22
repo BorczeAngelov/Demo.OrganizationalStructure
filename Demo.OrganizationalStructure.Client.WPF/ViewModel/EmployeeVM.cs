cbca4d8dd5cf02d86bd42d9626266459c82c45a7
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-23 01:30:16 +0200

    Add empty EmployeeVM class

---
+++ Demo.OrganizationalStructure.Client.WPF/ViewModel/EmployeeVM.cs
@@ -0,0 +1,15 @@
+﻿using Demo.OrganizationalStructure.Client.WPF.Utils;
+using Demo.OrganizationalStructure.Common.DataModel;
+
+namespace Demo.OrganizationalStructure.Client.WPF.ViewModel
+{
+    public class EmployeeVM : ObservableBase
+    {
+        private readonly Employee _dataModel;
+
+        internal EmployeeVM(Employee dataModel)
+        {
+            _dataModel = dataModel;
+        }
+    }
+}
