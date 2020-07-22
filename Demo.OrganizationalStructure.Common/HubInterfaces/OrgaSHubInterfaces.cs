819136af14279baab64180ebc4c3dc626a34a8b5
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-22 23:42:20 +0200

    Declare create, update and delete operations for JobRole data model

--- Demo.OrganizationalStructure.Common/HubInterfaces/OrgaSHubInterfaces.cs
+++ Demo.OrganizationalStructure.Common/HubInterfaces/OrgaSHubInterfaces.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo.OrganizationalStructure.Common.DataModel;
+using System;
 using System.Threading.Tasks;
 
 namespace Demo.OrganizationalStructure.Common.HubInterfaces
@@ -6,16 +7,28 @@ namespace Demo.OrganizationalStructure.Common.HubInterfaces
     public interface IOrgaSHub
     {
         Task PingDemo();
+
+        Task CreateJobRole(JobRole jobRole);
+        Task UpdateJobRole(JobRole jobRole);
+        Task DeleteJobRole(JobRole jobRole);
     }
 
     public interface IOrgaSHubClient
     {
         void InvokePongDemo();
+
+        void InvokeCreateJobRole(JobRole jobRole);
+        void InvokeUpdateJobRole(JobRole jobRole);
+        void InvokeDeleteJobRole(JobRole jobRole);
     }
 
     public interface IOrgaSHubClientTwoWayComm : IOrgaSHubClient
     {
         event Action PongedDemo;
+
+        event Action<JobRole> JobRoleCreated;
+        event Action<JobRole> JobRoleUpdated;
+        event Action<JobRole> JobRoleDeleted;
 
         IOrgaSHub ServerHubProxy { get; }
         Task ConnectWithServerHub();
