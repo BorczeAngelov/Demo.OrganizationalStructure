ba49161dcd82152c1fdc1f1e7e8b716a837548c3
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-23 01:30:03 +0200

    Add empty JobRoleVM class

---
+++ Demo.OrganizationalStructure.Client.WPF/ViewModel/JobRoleVM.cs
@@ -0,0 +1,15 @@
+﻿using Demo.OrganizationalStructure.Client.WPF.Utils;
+using Demo.OrganizationalStructure.Common.DataModel;
+
+namespace Demo.OrganizationalStructure.Client.WPF.ViewModel
+{
+    public class JobRoleVM : ObservableBase
+    {
+        private readonly JobRole _dataModel;
+
+        internal JobRoleVM(JobRole dataModel)
+        {
+            _dataModel = dataModel;
+        }
+    }
+}
