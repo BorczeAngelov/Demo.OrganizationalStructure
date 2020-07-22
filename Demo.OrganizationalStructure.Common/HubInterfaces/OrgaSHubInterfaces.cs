2f86da17e7b38fb70ba0d98c3079759a80a26884
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-22 22:15:53 +0200

    Define (empty) hub interfaces in single project file

---
+++ Demo.OrganizationalStructure.Common/HubInterfaces/OrgaSHubInterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Demo.OrganizationalStructure.Common.HubInterfaces
+{
+    public interface IOrgaSHub
+    {
+
+    }
+
+    public interface IOrgaSHubClient
+    {
+
+    }
+
+    public interface IOrgaSHubClientTwoWayComm : IOrgaSHubClient
+    {
+        IOrgaSHub ServerHubProxy { get; }
+        Task ConnectWithServerHub();
+    }
+}
