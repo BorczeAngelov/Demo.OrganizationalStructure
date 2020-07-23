a819018827bfba877aabb12afde5c647773c3856
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-23 05:48:11 +0200

    Add basic SelectedEditableItem view

---
+++ Demo.OrganizationalStructure.Client.WPF/View/SelectedEditableItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo.OrganizationalStructure.Client.WPF.View
+{
+    /// <summary>
+    /// Interaction logic for SelectedEditableItem.xaml
+    /// </summary>
+    public partial class SelectedEditableItem : UserControl
+    {
+        public SelectedEditableItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
