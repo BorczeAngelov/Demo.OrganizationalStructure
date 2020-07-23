9340143e8e6e0a5a3874bd22f05a3e2908ab5143
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-23 02:36:27 +0200

    Add basic properties, methods, event subscribtions etc.

--- Demo.OrganizationalStructure.Client.WPF/ViewModel/OrganizationalStructureVM.cs
+++ Demo.OrganizationalStructure.Client.WPF/ViewModel/OrganizationalStructureVM.cs
@@ -1,14 +1,55 @@
-﻿using Demo.OrganizationalStructure.Common.HubInterfaces;
+﻿using Demo.OrganizationalStructure.Client.WPF.Utils;
+using Demo.OrganizationalStructure.Common.DataModel;
+using Demo.OrganizationalStructure.Common.HubInterfaces;
+using System;
+using System.Collections.ObjectModel;
 
 namespace Demo.OrganizationalStructure.Client.WPF.ViewModel
 {
     public class OrganizationalStructureVM
     {
-        private readonly IOrgaSHubClientTwoWayComm _orgaSHubClientTwoWayComm;
+        private readonly IOrgaSHubClientTwoWayComm _twoWayComm;
 
-        internal OrganizationalStructureVM(IOrgaSHubClientTwoWayComm orgaSHubClientTwoWayComm)
+        internal OrganizationalStructureVM(IOrgaSHubClientTwoWayComm twoWayComm)
         {
-            _orgaSHubClientTwoWayComm = orgaSHubClientTwoWayComm;
+            _twoWayComm = twoWayComm;
+
+            AddJobRoleCommand = new DelegateCommand(CreateNewJobRole);
+            AddEmployeeCommand = new DelegateCommand(CreateNewEmployee);
+
+            _twoWayComm.JobRoleCreated += AddNewJobRoleFromServer;
+            _twoWayComm.EmployeeCreated += AddNewEmployeeFromServer;
+        }
+
+        public DelegateCommand AddJobRoleCommand { get; }
+        public DelegateCommand AddEmployeeCommand { get; }
+        public ObservableCollection<JobRoleVM> JobRoles { get; } = new ObservableCollection<JobRoleVM>();
+        public ObservableCollection<EmployeeVM> Employees { get; } = new ObservableCollection<EmployeeVM>();
+
+        private void CreateNewJobRole(object obj)
+        {
+            var dataModel = new JobRole() { Name = "New job role", EntityKey = Guid.NewGuid() };
+            var jobRoleVM = new JobRoleVM(_twoWayComm, dataModel, isNewAndUnsaved: true);
+            JobRoles.Add(jobRoleVM);
+        }
+
+        private void CreateNewEmployee(object obj)
+        {
+            var dataModel = new Employee() { Name = "New employee", EntityKey = Guid.NewGuid() };
+            var employeeVM = new EmployeeVM(_twoWayComm, dataModel, isNewAndUnsaved: true);
+            Employees.Add(employeeVM);
+        }
+
+        private void AddNewJobRoleFromServer(JobRole jobRole)
+        {
+            var jobRoleVM = new JobRoleVM(_twoWayComm, jobRole);
+            JobRoles.Add(jobRoleVM);
+        }
+
+        private void AddNewEmployeeFromServer(Employee employee)
+        {
+            var employeeVM = new EmployeeVM(_twoWayComm, employee);
+            Employees.Add(employeeVM);
         }
     }
 }
