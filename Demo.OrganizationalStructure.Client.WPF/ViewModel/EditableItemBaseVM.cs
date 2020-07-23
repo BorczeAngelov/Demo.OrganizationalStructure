071c3a34315c96d18be6bf5e043f1f7232f45546
Borcze Angelov <borcze.angelov@yahoo.com>
2020-07-23 06:07:55 +0200

    Declare cancel and delete commands

--- Demo.OrganizationalStructure.Client.WPF/ViewModel/EditableItemBaseVM.cs
+++ Demo.OrganizationalStructure.Client.WPF/ViewModel/EditableItemBaseVM.cs
@@ -1,11 +1,13 @@
 ﻿using Demo.OrganizationalStructure.Client.WPF.Utils;
 using Demo.OrganizationalStructure.Common.HubInterfaces;
+using System;
 
 namespace Demo.OrganizationalStructure.Client.WPF.ViewModel
 {
     public abstract class EditableItemBaseVM : ObservableBase
     {
         protected readonly IOrgaSHubClientTwoWayComm TwoWayComm;
+        private bool _isNewAndUnsaved;
 
         protected EditableItemBaseVM(
             IOrgaSHubClientTwoWayComm twoWayComm,
@@ -18,7 +20,21 @@ namespace Demo.OrganizationalStructure.Client.WPF.ViewModel
         }
 
         public DelegateCommand SaveCommand { get; }
-        public bool IsNewAndUnsaved { get; private set; }
+        public DelegateCommand CancelCommand { get /*=> throw new NotImplementedException()*/; }
+        public DelegateCommand DeleteCommand { get /*=> throw new NotImplementedException()*/; }
+
+        public bool IsNewAndUnsaved
+        {
+            get => _isNewAndUnsaved;
+            private set
+            {
+                if (_isNewAndUnsaved != value)
+                {
+                    _isNewAndUnsaved = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         protected abstract void SaveNew();
         protected abstract void Save();
