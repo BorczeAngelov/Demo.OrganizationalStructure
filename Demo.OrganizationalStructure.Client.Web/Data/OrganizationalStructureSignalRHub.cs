fe493ded556e3e24bd2e00994d0ee4a268efe0f6
Borcze Angelov <borcze.angelov@yahoo.com>
2021-02-21 14:42:19 +0100

    Connect to server using SignalR Hub

---
+++ Demo.OrganizationalStructure.Client.Web/Data/OrganizationalStructureSignalRHub.cs
@@ -0,0 +1,36 @@
+﻿using Demo.OrganizationalStructure.Common.DataModel;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo.OrganizationalStructure.Client.Web.Data
+{
+    internal class OrganizationalStructureSignalRHub
+    {
+        private readonly HubConnection _serverConnection;
+
+        internal event Action<Organisation> LoadOrganisation;
+
+        public OrganizationalStructureSignalRHub()
+        {
+            var serverHubUrl = "http://localhost:5000/OrgaSHub";
+
+            _serverConnection = new HubConnectionBuilder()
+                .WithUrl(serverHubUrl)
+                .WithAutomaticReconnect()
+                .Build();
+
+            _serverConnection.On<Organisation>(nameof(InvokeLoadOrganisation), InvokeLoadOrganisation);
+        }
+
+        private void InvokeLoadOrganisation(Organisation organisation)
+        {
+            LoadOrganisation?.Invoke(organisation);
+        }
+
+        internal async Task ConnectWithServerAsync()
+        {
+            await _serverConnection.StartAsync();
+        }
+    }
+}
